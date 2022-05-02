0965915381825b9094fc4c053662305fa35e8c64
GrincenkoDaria <grincenkodaria878@gmail.com>
2022-05-02 20:41:24 +0200

    moneyall

--- Pomoc.js
+++ Pomoc.js
@@ -10,4 +10,14 @@ function add_r(i1) {
 	a.value = a.value + i1;
 }
 
+		
+function f_Date(el,n) {
+
+	el.value = el.value.replace(/[^0-9]/g,'');
+
+	if(el.value.length>n)el.value=el.value.substr(n);
+
+	if(el.value.length==n) el.nextSibling.nextSibling.focus();
+
+}
 
